fa4da2f90605a1bf3926ddb4cd954d1435f1e981
maxiw <max.iw15@yandex.ru>
2024-06-14 17:23:56 +0300

    Registration form fix

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-
 import { Container, Center, TextInput, PasswordInput, Space, Button, Anchor } from '@mantine/core';
-
 import { login, signUp } from '../redux/slices/authSlice';
-
 
 function Auth({ action }) {
   const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
   const dispatch = useDispatch();
-  const [error, setError] = useState();
+
+  const validateUsername = (username) => /^[a-zA-Z][a-zA-Z0-9]{3,19}$/.test(username);
+  const validateEmail = (email) => /^[^\s@]+@[^\s@]+.[^\s@]+$/.test(email);
+  const validatePassword = (password) => /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/.test(password);
 
   const handleLogin = async () => {
     const response = await dispatch(login({ username, password }));
@@ -24,37 +30,100 @@ function Auth({ action }) {
   };
 
   const handleSignUp = async () => {
-    const response = await dispatch(signUp({ username, password }));
+    let valid = true;
+    if (!validateUsername(username)) {
+      setUsernameError('Invalid username format.');
+      valid = false;
+    } else {
+      setUsernameError('');
+    }
+
+    if (!validateEmail(email)) {
+      setEmailError('Invalid email format.');
+      valid = false;
+    } else {
+      setEmailError('');
+    }
+
+    if (!validatePassword(password)) {
+      setPasswordError('Invalid password format.');
+      valid = false;
+    } else {
+      setPasswordError('');
+    }
+
+    if (!valid) {
+      return;
+    }
+
+    const response = await dispatch(signUp({ username, fullName, email, password }));
     if (signUp.fulfilled.match(response)) {
       navigate('/');
     } else {
       setError('Sign up failed. Try again with another username.');
     }
-  }
-
+  };
 
   return (
     <Center h={700}>
       <Container>
-        <TextInput
-          style={{ width: '400px' }}
-          label="Login"
-          size='md'
-          value={username}
-          {...(error && { error: error })}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <Space h="lg" />
+        {action === 'signup' && (
+          <>
+            <TextInput
+              style={{ width: '400px' }}
+              label="Username"
+              size='md'
+              value={username}
+              error={usernameError}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Space h="lg" />
+            <TextInput
+              style={{ width: '400px' }}
+              label="Full Name"
+              size='md'
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+            <Space h="lg" />
+            <TextInput
+              style={{ width: '400px' }}
+              label="Email"
+              size='md'
+              value={email}
+              error={emailError}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Space h="lg" />
+          </>
+        )}
+        {action === 'signin' && (
+          <>
+            <TextInput
+              style={{ width: '400px' }}
+              label="Username"
+              size='md'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Space h="lg" />
+          </>
+        )}
         <PasswordInput
           style={{ width: '400px' }}
           size="md"
           label="Password"
           value={password}
-          {...(error && { error: error })}
+          error={passwordError}
           onChange={(e) => setPassword(e.target.value)}
-          onKeyPress={(e) => e.key === 'Enter' && handleLogin()}
+          onKeyPress={(e) => e.key === 'Enter' && (action === 'signin' ? handleLogin() : handleSignUp())}
         />
-        <Space h="xl" />
+ <Space h="xl" />
+        {error && (
+          <Center>
+            <div style={{ color: 'red' }}>{error}</div>
+          </Center>
+        )}
         {action === 'signin' ? (
           <>
             <Center>
@@ -64,7 +133,7 @@ function Auth({ action }) {
             </Center>
             <Space h="md" />
             <Center>
-              <Anchor href="signup">
+              <Anchor href="/signup">
                 Sign up
               </Anchor>
             </Center>
@@ -80,6 +149,5 @@ function Auth({ action }) {
     </Center>
   );
 }
-
 
 export default Auth;
